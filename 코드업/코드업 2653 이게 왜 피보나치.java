a902e800a86326f978f3a0719c97bb3b136ee31b
SeunghyunDo <58718180+dddoseung@users.noreply.github.com>
2020-11-17 20:00:00 +0900

    Update 코드업 2653 이게 왜 피보나치.java

--- 코드업/코드업 2653 이게 왜 피보나치.java
+++ 코드업/코드업 2653 이게 왜 피보나치.java
@@ -1,4 +1,5 @@
-﻿import java.util.*;
+// 방법1: 재귀와 메모이제이션을 이용한 피보나치 풀이
+import java.util.*;
 
 public class Main{
     
@@ -32,3 +33,26 @@ public static int fibo(int n){
     
 }
 }
+
+// 방법2: 반복문을 이용한 피보나치 풀이
+//import java.io.*;
+
+// public class Main {
+// 	static int[] arr; // 피보나치 수를 담을 배열
+	
+// 	public static void main(String[] args) throws IOException{
+// 		// TODO Auto-generated method stub
+// 		 BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+// 	        int n = Integer.parseInt(reader.readLine());
+// 	        arr=new int[n+1];
+	        
+// 	        arr[0]=2;
+// 	        arr[1]=3;
+	        
+// 	        for(int i=2;i<n;i++){
+// 	            arr[i]=arr[i-1]+arr[i-2];
+// 	        }
+	        
+// 	        System.out.println(arr[n-1]);
+// 	}
+// }
