63aba5aa044ef9c337261dd5b46fd2f86ca21c2a
SeunghyunDo <58718180+dddoseung@users.noreply.github.com>
2020-11-20 19:18:14 +0900

    Add files via upload

--- 백준알고리즘/백준 Q12015 LIS(이분탐색)/src/Q12015.java
+++ 백준알고리즘/백준 Q12015 LIS(이분탐색)/src/Q12015.java
@@ -1,51 +1,40 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
-//LIS
+
 public class Q12015 {
-	static int N; // 수열의 크기 N
-	static int[] arr; // 수열을 담은 배열
-	
-	static int[] dp_LIS; // 메모이제이션
-	static int[] visit_LIS; // dp를 방문했는가(저장되어있는가)를 담는 배열
-    
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
     public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		
-        N=Integer.parseInt(br.readLine());
-		arr=new int[N];
-		
-		dp_LIS=new int[N];
-		visit_LIS=new int[N]; 
-		
-		StringTokenizer st=new StringTokenizer(br.readLine());
-		for(int i=0;i<N;i++) {
-			arr[i]=Integer.parseInt(st.nextToken());
-			dp_LIS[i]=1; // 1로 초기화**
-		}
-        
-        int max=0;
-        for(int i=0;i<N;i++){
-            max=Math.max(LIS(arr[i]),max);
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        List<Integer> list = new ArrayList<>();
+        list.add(0);
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        for(int i = 0 ; i < n; i++) {
+            int value = arr[i] = Integer.parseInt(st.nextToken());
+            if(value > list.get(list.size() - 1)) list.add(value);
+            else{
+                int left = 0;
+                int right = list.size() - 1;
+
+                while(left < right){
+                    int mid = (left + right) >> 1;
+                    if(list.get(mid) >= value){
+                        right = mid;
+                    }else{
+                        left = mid + 1;
+                    }
+                }
+                list.set(right, value);
+            }
         }
-        System.out.print(max);
-        
+        System.out.println(list.size() - 1);
     }
-    public static int LIS(int num){
-        
-        if(visit_LIS[num]!=0){
-            return dp_LIS[num];
-        }
-        else{
-            visit_LIS[num]=1;
-            for(int i=0;i<num;i++){
-                if(arr[num]>arr[i])
-                    dp_LIS[num]=Math.max(dp_LIS[num],LIS(i)+1);
-            } 
-            return dp_LIS[num];
-        }
-        
-    }
+
 }
