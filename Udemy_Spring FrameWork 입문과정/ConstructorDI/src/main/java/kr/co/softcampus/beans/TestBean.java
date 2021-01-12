3b348825328974a3fa8fb78daf1fc0d5ec7cd095
SeunghyunDo <58718180+dddoseung@users.noreply.github.com>
2021-01-12 23:37:49 +0900

    Add files via upload

--- Udemy_Spring FrameWork 입문과정/ConstructorDI/src/main/java/kr/co/softcampus/beans/TestBean.java
+++ Udemy_Spring FrameWork 입문과정/ConstructorDI/src/main/java/kr/co/softcampus/beans/TestBean.java
@@ -12,6 +12,34 @@ public class TestBean {
 		this.data3 = null;
 	}
 
+	public TestBean(int data1) {
+		System.out.println("TestBean의 생성자: int 변수 한개");
+		this.data1=data1;
+		this.data2=0.0;
+		this.data3=null;
+	}
+	
+	public TestBean(double data2) {
+		System.out.println("TestBean의 생성자: double 변수 한개");
+		this.data1=0;
+		this.data2=data2;
+		this.data3=null;
+	}
+	
+	public TestBean(String data3) {
+		System.out.println("TestBean의 생성자: String 변수 한개");
+		this.data1=0;
+		this.data2=0.0;
+		this.data3=data3;
+	}
+	
+	public TestBean(int data1, double data2, String data3) {
+		System.out.println("TestBean의 생성자: 변수 3개");
+		this.data1=data1;
+		this.data2=data2;
+		this.data3=data3;
+	}
+	
 	public void printData() {
 		System.out.printf("data1: %d\n", data1);
 		System.out.printf("data2: %f\n", data2);
